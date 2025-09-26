28f3b3bafb21b12d05ceb654ea1e8b24e38e4e76
rotarualexandru22 <rotarualexandru22@yahoo.com>
2025-09-26 16:20:56 +0300

    Done contactus, ourwork and teams components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
+import OurWork from './components/OurWork'
+import Teams from './components/Teams'
+import ContactUs from './components/ContactUs'
 
 const App = () => {
 
@@ -15,6 +18,9 @@ const App = () => {
       <Hero />
       <TrustedBy />
       <Services />
+      <OurWork />
+      <Teams />
+      <ContactUs />
     </div>
   )
 }
