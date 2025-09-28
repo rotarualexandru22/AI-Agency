e76e16f7b1dd54ddb17a1e24d25f6fbab1c66965
rotarualexandru22 <rotarualexandru22@yahoo.com>
2025-09-28 19:22:24 +0300

    Done, ContactUs needs fixing

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
 import toast from 'react-hot-toast'
+import { motion } from 'motion/react'
 
 const ContactUs = () => {
 
@@ -32,12 +33,22 @@ const ContactUs = () => {
     }
 
   return (
-    <div id='contact-us' className='flex flex-col items-center gap-7 sm:px-12
+    <motion.div 
+     initial="hidden"
+     whileInView="visible"
+     viewport={{once: true}}
+     transition={{staggerChildren: 0.2}}
+    id='contact-us' className='flex flex-col items-center gap-7 sm:px-12
     lg:px-24 xl:px-40 pt-38 text-gray-700 dark:text-white'>
         <Title title='Reach out to us' desc='From strategy to execution, we craft
         digital solutions that move your business forward'/>
 
-        <form onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+        <motion.form 
+        initial={{opacity: 0, y: 30}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.4}}
+        viewport={{once: true}}
+        onSubmit={onSubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
             
             <div>
                 <p className='mb-2 text-sm font-medium'>Your name</p>
@@ -71,8 +82,8 @@ const ContactUs = () => {
                 Submit <img src={assets.arrow_icon} alt="" className='w-4'/>
             </button>
 
-        </form>
-    </div>
+        </motion.form>
+    </motion.div>
   )
 }
 
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react'
+import { motion } from 'motion/react'
 
 export const ServiceCard = ({service, index}) => {
 
@@ -13,7 +14,12 @@ export const ServiceCard = ({service, index}) => {
     }
 
   return (
-    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl
+    <motion.div 
+        initial={{opacity: 0, y: 30}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: index * 0.2}}
+        viewport={{once: true}}
+    className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl
     border border-gray-200 dark:border-gray-700 shadow-2xl shadow:gray-100
     dark:shadow-white/10' onMouseEnter={() => setVisible(true)} 
     onMouseLeave={() => setVisible(false)} ref={divRef} onMouseMove={handleMouseMove}>
@@ -37,7 +43,7 @@ export const ServiceCard = ({service, index}) => {
 
             </div>
 
-        </div>
+        </motion.div>
 
   )
 }
